75d7a444f8f18efbf45273181c1123a54635fd1d
大大白 <1242251966@qq.com>
2021-07-10 16:09:45 +0800

    Update client.py

--- tg_client/client.py
+++ tg_client/client.py
@@ -5,14 +5,13 @@ client = TelegramClient('simple_user_bot', api_id, api_hash)
 client.connect()
 
 
-
-def get_all_group_member(chat_id):
-    all_participants = client.get_participants(chat_id, aggressive=True)
-    all_group_member_list = []
-    for index in range(len(all_participants)):
-        all_group_member_dict = {'ID': all_participants[index].id, 'Username': all_participants[index].username,'Language': all_participants[index].lang_code, 'Bot': all_participants[index].bot}
-        all_group_member_list.append(all_group_member_dict)
+def get_all_group_member(chat_id, count):
+    all_participants = client.get_participants(chat_id)
+    for index in range(count):
+        all_group_member_dict = {'ID': all_participants[index].id, 'Username': all_participants[index].username,
+                                     'Language': all_participants[index].lang_code, 'Bot': all_participants[index].bot}
         print(all_group_member_dict)
-    return all_group_member_list
+    # database.write_data("group_member", chat_id, all_group_member_dict)
 
-get_all_group_member(-1001266485204)
+
+# get_all_group_member(-1001196317569, 11)
