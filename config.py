ae371f6a0957edeb8271ddb31ad8410d22dfbfb9
大大白 <1242251966@qq.com>
2021-07-14 21:01:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ if ENV:
     BOT_NAME = os.environ.get("BOT_NAME", None)
     AllGroupMemberDatabaseName = os.environ.get("AllGroupMemberDatabaseName", None)
 else:
-    TOKEN = '1826253371:AAFbV-ECTwwg2r7PZ-T1TIrg8tO9vsqG1rg'
+    TOKEN = ''
     BanMeReplayAddress = "https://ae02.alicdn.com/kf/U54e2d33aa0694530897b008b097a5fb5U.png"
-    api_id = 6692291
-    api_hash = 'd37e4b6bb7af5e69e842ac0bcbab7e74'
-    BOT_NAME = 'DH_TG_Bot'
-    AllGroupMemberDatabaseName = 'group_data'
+    api_id = 
+    api_hash = ''
+    BOT_NAME = ''
+    AllGroupMemberDatabaseName = ''
